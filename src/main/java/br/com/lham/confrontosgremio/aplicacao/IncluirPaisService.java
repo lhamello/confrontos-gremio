12284ea7bde9a32d4a0047d7b70bd77d623adb01
Luiz Henrique A. Mello <lhamello@gmail.com>
2019-12-23 19:06:34 -0300

    Deletado classes não utilizadas.

--- src/main/java/br/com/lham/confrontosgremio/aplicacao/IncluirPaisService.java
+++
@@ -1,6 +0,0 @@
-package br.com.lham.confrontosgremio.aplicacao;
-
-public interface IncluirPaisService {
-
-  void incluir(final PaisDTO paisDTO);
-}
--- src/main/java/br/com/lham/confrontosgremio/aplicacao/PaisDTO.java
+++
@@ -1,5 +0,0 @@
-package br.com.lham.confrontosgremio.aplicacao;
-
-public class PaisDTO {
-
-}
