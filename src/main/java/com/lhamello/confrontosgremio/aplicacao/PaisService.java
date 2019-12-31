caac4726a5856d998e96c03886587df8bfcad794
Luiz Henrique A. Mello <lhamello@gmail.com>
2019-12-31 15:54:39 -0300

    Resolvido conflito.

--- src/main/java/com/lhamello/confrontosgremio/aplicacao/PaisService.java
+++ src/main/java/com/lhamello/confrontosgremio/aplicacao/PaisService.java
@@ -4,9 +4,5 @@ import com.lhamello.confrontosgremio.dominio.model.pais.Pais;
 
 public interface PaisService {
 
-<<<<<<< HEAD
-  abstract Pais cadastrarPais(final String abreviatura, final String nome, final Continente continente);
-=======
   abstract Pais cadastrarPais(final String abreviaturaPais, final String nomePais, final String abreviaturaContinente);
->>>>>>> branch 'master' of https://github.com/lhamello/confrontos-gremio.git
 }
