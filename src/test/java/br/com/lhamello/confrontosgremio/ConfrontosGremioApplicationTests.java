b7abdac45e21b7ff1947e9164a06ed603af359fc
Luiz Henrique A. Mello <lhamello@gmail.com>
2021-03-01 01:11:42 -0300

    Desabilitado teste padrão do spring boot.

--- src/test/java/br/com/lhamello/confrontosgremio/ConfrontosGremioApplicationTests.java
+++ src/test/java/br/com/lhamello/confrontosgremio/ConfrontosGremioApplicationTests.java
@@ -1,12 +1,11 @@
 package br.com.lhamello.confrontosgremio;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class ConfrontosGremioApplicationTests {
+class ConfrontosGremioApplicationTests {
 
-  @Test
-  void contextLoads() {
-  }
+//  @Test
+//  void contextLoads() {
+//  }
 }
